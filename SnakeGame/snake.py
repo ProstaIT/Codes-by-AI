b08e345773eacee271983fea0743330b8bdc6623
ITPROSTA <81316740+ProstaIT@users.noreply.github.com>
2021-12-08 09:49:15 +0100

    Update snake.py

--- SnakeGame/snake.py
+++ SnakeGame/snake.py
@@ -1,4 +1,9 @@
 #snake game
+#Author of the code - ITProsta
+#Donat - https://www.donationalerts.com/r/prostaprogramming
+#https://github.com/ProstaIT
+#https://www.youtube.com/c/ITPROSTA
+    
 import pygame, sys, random
 from pygame.locals import *
 
